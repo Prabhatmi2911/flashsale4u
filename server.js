e3850d5062fa5d468ab6e26f6129a1a15367f99c
Prabhatmi2911 <www.prabhatmishra1234@gmail.com>
2025-09-16 10:07:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home page
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// 5G Phones page
+app.get('/phones', (req, res) => {
+    fs.readFile('5g_phone.json', 'utf8', (err, data) => {
+        if (err) return res.status(500).send('Server Error');
+        const products = JSON.parse(data);
+        res.render('phones', { products });
+    });
+});
+
+// Laptops page
+app.get('/laptops', (req, res) => {
+    fs.readFile('laptop.json', 'utf8', (err, data) => {
+        if (err) return res.status(500).send('Server Error');
+        const products = JSON.parse(data);
+        res.render('laptops', { products });
+    });
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
